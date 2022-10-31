ca9018e5428b4241e40e67766de2b4a4f633e8d5
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-10-31 23:18:42 +0530

    Added :Display Welcome to LinkedList Assignment

---
+++ src/com/linkedlist/problems/LinkedList.java
@@ -0,0 +1,10 @@
+//This class is all about LinkedList Problem.
+
+package com.linkedlist.problems;
+
+public class LinkedList {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to linked list Problems");
+    }
+}
